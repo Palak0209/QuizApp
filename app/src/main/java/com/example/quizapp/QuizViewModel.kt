b1b140cae37212c92ddcc85cd1801865278aaef0
Palak <123983133+Palak0209@users.noreply.github.com>
2024-11-28 20:36:36 -0500

    Timer implemented for viewmodel

--- app/src/main/java/com/example/quizapp/QuizViewModel.kt
+++ app/src/main/java/com/example/quizapp/QuizViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.quizapp
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.quizapp.ui.theme.ThemeSettings
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -10,6 +13,12 @@ class QuizViewModel(private val settingsDataStore: SettingsDataStore) : ViewMode
 
     var isLightTheme = mutableStateOf(ThemeSettings.isLightTheme)
         private set
+    var isTimerEnabled = mutableStateOf(false) // Tracks whether the timer is enabled
+        private set
+
+    val timerDuration = 10
+    var remainingTime = mutableStateOf(timerDuration)
+    private var timerJob: Job? = null // Coroutine job for the timer
 
     private val allQuestions = Questions.questions
     var currentQuestionIndex = mutableStateOf(0)
@@ -34,6 +43,11 @@ class QuizViewModel(private val settingsDataStore: SettingsDataStore) : ViewMode
                 highScore.value = score
             }
         }
+        viewModelScope.launch {
+            settingsDataStore.isTimerEnabled.collectLatest { enabled ->
+                isTimerEnabled.value = enabled
+            }
+        }
     }
 
     fun changeTheme() {
@@ -48,6 +62,33 @@ class QuizViewModel(private val settingsDataStore: SettingsDataStore) : ViewMode
     fun checkAnswer(selectedAnswer: String) {
         if(selectedAnswer == currentQuestion.answer)
             currentScore.value += 1
+    }
+
+    fun toggleTimer() {
+        val newState = !isTimerEnabled.value
+        isTimerEnabled.value = newState
+        viewModelScope.launch {
+            settingsDataStore.saveTimerState(newState)
+        }
+    }
+
+    fun startQuizTimer(onTimeUp: () -> Unit) {
+        if (!isTimerEnabled.value) return
+        timerJob?.cancel() // Cancel any existing timer
+        timerJob = viewModelScope.launch {
+            for (time in timerDuration downTo 0) {
+                remainingTime.value = time
+                delay(1000L)
+            }
+
+            Log.d("24601", "text")
+            onTimeUp() // Trigger the navigation when time is up
+        }
+    }
+
+    fun cancelQuizTimer() {
+        timerJob?.cancel()
+        remainingTime.value = timerDuration
     }
 
     fun nextQuestion() {
